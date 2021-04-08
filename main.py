0c1b5d35206b0b681b2301d31fbb121de2170864
Denis Mandic <dmandi51@gmail.com>
2021-04-08 17:50:07 +0200

    Complicate things with DB and ORM.

--- main.py
+++ main.py
@@ -2,10 +2,24 @@
 
 from flask import Flask
 from flask import render_template
+from flask_sqlalchemy import SQLAlchemy
 
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='web/templates', static_folder='web/static')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
+
+class Item(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(length=30), nullable=False, unique=True)
+    price = db.Column(db.Integer(), nullable=False)
+    barcode = db.Column(db.String(length=12), nullable=False, unique=True)
+    description = db.Column(db.String(length=1024), nullable=False)
+
+    def __repr__(self):
+        return f'Item {self.name}'
 
 @app.route('/')
 def home_page():
@@ -14,16 +28,27 @@ def home_page():
 
 @app.route('/market')
 def market_page():
-    items = [
-        {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
-        {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
-        {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150},
-        {'id': 4, 'name': 'Mouse', 'barcode': '478522128446', 'price': 100},
-        {'id': 5, 'name': 'Dog', 'barcode': '233347868446', 'price': 200},
-        {'id': 6, 'name': 'Table', 'barcode': '968950128446', 'price': 50},
-    ]
+    items = Item.query.all()
     return render_template('market.html', items=items)
 
 
+ITEMS = [
+    {'name': 'Phone', 'barcode': '893212299897', 'price': 500, 'description': 'The best phone in the world.'},
+    {'name': 'Laptop', 'barcode': '123985473165', 'price': 900, 'description': 'The best laptop in the world.'},
+    {'name': 'Keyboard', 'barcode': '231985128446', 'price': 150, 'description': 'The best keyboard in the world.'},
+    {'name': 'Mouse', 'barcode': '478522128446', 'price': 100, 'description': 'Not a pet.'},
+    {'name': 'Dog', 'barcode': '233347868446', 'price': 200, 'description': 'A pet.'},
+    {'name': 'Table', 'barcode': '968950128446', 'price': 50, 'description': 'DROP TABLES'},
+]
+
+def populate_db():
+    db.create_all()
+    for item in ITEMS:
+        db_item = Item(**item)
+        db.session.add(db_item)
+    db.session.commit()
+
+
 if __name__ == '__main__':
+    populate_db()
     app.run(debug=True)
