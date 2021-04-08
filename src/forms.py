f458b80a915047d207d36368d90efaaedec99edd
Denis Mandic <dmandi51@gmail.com>
2021-04-08 22:05:51 +0200

    Add some basic form validation.

--- src/forms.py
+++ src/forms.py
@@ -1,13 +1,26 @@
-
 from wtforms import StringField
 from wtforms import SubmitField
 from wtforms import PasswordField
+from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError
 from flask_wtf import FlaskForm
 
+from src.database import User
 
 class RegisterForm(FlaskForm):
-    username = StringField(label='User name:')
-    email_address = StringField(label='Email address:')
-    password_initial = PasswordField(label='Password:')
-    password_confirmation = PasswordField(label='Confirm password:')
+
+    def validate_username(self, username_to_check):  ## FlaskForm specific behaviour
+        user = User.query.filter_by(username=username_to_check.data)
+        if user.first():
+            raise ValidationError('Username already exists.')
+
+    def validate_email_address(self, email_to_check):
+        email =  User.query.filter_by(email_address=email_to_check.data)
+        if email.first():
+            raise ValidationError('Email already in use.')
+
+    username = StringField(label='User name:', validators=[Length(min=2, max=30), DataRequired()])
+    email_address = StringField(label='Email address:', validators=[Email(), DataRequired()])
+    password_initial = PasswordField(label='Password:', validators=[Length(min=6), DataRequired()])
+    password_confirmation = PasswordField(label='Confirm password:',
+                                          validators=[EqualTo('password_initial'), DataRequired()])
     submit = SubmitField(label='Create Account')
--- src/routes.py
+++ src/routes.py
@@ -1,8 +1,13 @@
 
+from flask import flash
+from flask import url_for
+from flask import redirect
 from flask import render_template
 
+from src import db
 from src import app
 from src.database import Item
+from src.database import User
 from src.forms import RegisterForm
 
 
@@ -18,9 +23,21 @@ def market_page():
     return render_template('market.html', items=items)
 
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register_page():
     registration_form = RegisterForm()
     if registration_form.validate_on_submit():
-        pass
+        user_to_create = User(username=registration_form.username.data,
+                              email_address=registration_form.email_address.data,
+                              password_hash=registration_form.password_initial.data)
+        db.session.add(user_to_create)
+        db.session.commit()
+
+        market_page_url = url_for('market_page')
+        return redirect(market_page_url)
+
+    if registration_form.errors:
+        for err_msg in registration_form.errors.values():
+            flash(err_msg, category='danger')
+
     return render_template('register.html', form=registration_form)
